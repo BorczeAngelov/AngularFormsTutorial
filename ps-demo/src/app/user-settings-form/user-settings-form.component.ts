83425071991000a599adaca85fe9e6dfd4c84f49
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-09-28 23:33:55 +0200

    use dataService to get an empty Observable

--- ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -18,13 +19,20 @@ export class UserSettingsFormComponent implements OnInit {
   }
 
   userSettings: UserSettings = { ...this.originalUserSettings }
-  constructor() { }
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: NgForm) {
     console.log('in onSumbmit: ', form.valid)
+
+    this.dataService.postUserSettingsForm(this.userSettings).subscribe(      
+        result => console.log('result from observable = ', result),
+        error => console.log('error: ', error)        
+    );
+
   }
 
   onBlur(field: NgModel) {
